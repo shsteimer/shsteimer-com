33a5613c2c3a5fa7af56ac7f9e53e49cc02698f0
Sean Steimer <ssteimer@adobe.com>
2024-02-22 14:44:35 -0800

    delay fixes

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,16 +1,20 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM, loadCSS, loadScript } from './aem.js';
 
-const runDelayed = async () => {
-  // Core Web Vitals RUM collection
-  sampleRUM('cwv');
-
+const highlightCode = async () => {
   if (document.querySelector('.code:not(.gist)')) {
     await loadCSS('../../libs/highlight/styles/github-dark.min.css');
     await loadScript('../../libs/highlight/highlight.min.js');
     // eslint-disable-next-line no-undef
     hljs.highlightAll();
   }
+};
+
+const runDelayed = async () => {
+  // Core Web Vitals RUM collection
+  sampleRUM('cwv');
+
+  await highlightCode();
 };
 
 // add more delayed functionality here
