8c700d9d33021c93a92e333b57aca2d7d6b10d26
Sean Steimer <ssteimer@adobe.com>
2024-02-23 11:44:04 -0800

    chore: view transition search results

--- blocks/posts/posts.js
+++ blocks/posts/posts.js
@@ -17,35 +17,70 @@ export function createPostDate(post) {
   return p({ class: 'post-date' }, `Posted on ${dateFormatted} in `, a({ href: `/blog/archives#categories:${toClassName(post.category)}` }, post.category));
 }
 
-async function renderResults(block, results) {
-  block.innerHTML = '';
-  const posts = ul({ class: 'posts-list' });
-  block.append(posts);
-  // eslint-disable-next-line no-restricted-syntax
-  for await (const res of results) {
-    const d = new Date(0);
-    d.setUTCSeconds(res.date);
+let lastQ;
+async function renderResults(block, results, q = '') {
+  if (lastQ === q.trim()) return;
+  lastQ = q.trim();
 
-    const post = li(
-      { class: 'post-item' },
-      a({ href: res.path, class: 'post-title' }, h2(res.title)),
-      createPostDate(res),
-      p({ class: 'post-description' }, res.description),
-      a({ href: res.path }, 'Read More...'),
-    );
-    posts.append(post);
+  const render = async () => {
+    const posts = block.querySelector('.posts-list') || ul({ class: 'posts-list' });
+    posts.innerHTML = '';
+    block.append(posts);
+    // eslint-disable-next-line no-restricted-syntax
+    for await (const res of results) {
+      const d = new Date(0);
+      d.setUTCSeconds(res.date);
+
+      const post = li(
+        { class: 'post-item' },
+        a({ href: res.path, class: 'post-title' }, h2(res.title)),
+        createPostDate(res),
+        p({ class: 'post-description' }, res.description),
+        a({ href: res.path }, 'Read More...'),
+      );
+      posts.append(post);
+    }
+
+    if (q.trim()) {
+      if (block.querySelector('.posts-list:empty')) {
+        block.querySelector('.posts-list:empty').append(li({ class: 'post-item' }, 'No Results Found'));
+      } else {
+        block.querySelectorAll('.post-item').forEach((item) => {
+          const regex = new RegExp(q.trim(), 'idg');
+          const title = item.querySelector('h2');
+          title.innerHTML = title.innerHTML.replaceAll(regex, (match) => `<mark>${match}</mark>`);
+
+          const category = item.querySelector('.post-date a');
+          category.innerHTML = category.innerHTML.replaceAll(regex, (match) => `<mark>${match}</mark>`);
+
+          const description = item.querySelector('.post-description');
+          description.innerHTML = description.innerHTML.replaceAll(regex, (match) => `<mark>${match}</mark>`);
+        });
+      }
+    }
+  };
+  if (!document.startViewTransition) {
+    await render();
+  } else {
+    document.startViewTransition(() => render());
   }
 }
+
+async function renderInitialResults(block) {
+  const results = ffetch('/query-index.json')
+    .filter((page) => page.template && page.template.toLowerCase() === 'post')
+    .limit(5);
+
+  renderResults(block, results);
+}
+
 /**
  * decorate the block
  * @param {Element} block the block
  */
 export default async function decorate(block) {
-  const results = ffetch('/query-index.json')
-    .filter((page) => page.template && page.template.toLowerCase() === 'post')
-    .limit(5);
-
-  await renderResults(block, results);
+  block.innerHTML = '';
+  renderInitialResults(block);
 
   // search interaction
   const header = document.querySelector('header');
@@ -56,36 +91,21 @@ export default async function decorate(block) {
   window.addEventListener('message', (msg) => {
     if (msg.origin === getOrigin() && msg.data && msg.data.executeSearch) {
       const { q } = msg.data;
-      const regex = new RegExp(q, 'idg');
-      const searchResults = ffetch('/query-index.json')
-        .filter((page) => {
-          const isPost = page.template && page.template.toLowerCase() === 'post';
-          if (!isPost) return false;
+      if (!q.trim()) {
+        renderInitialResults(block);
+        return;
+      }
+      const regex = new RegExp(q.trim(), 'idg');
+      const searchResults = ffetch('/query-index.json').filter((page) => {
+        const isPost = page.template && page.template.toLowerCase() === 'post';
+        if (!isPost) return false;
 
-          if (q) {
-            return regex.test(page.title)
-              || regex.test(page.description)
-              || regex.test(page.category);
-          }
-
-          return true;
-        });
-      renderResults(block, searchResults).then(() => {
-        if (block.querySelector('.posts-list:empty')) {
-          block.querySelector('.posts-list:empty').append(li({ class: 'post-item' }, 'No Results Found'));
-        } else {
-          block.querySelectorAll('.post-item').forEach((item) => {
-            const title = item.querySelector('h2');
-            title.innerHTML = title.innerHTML.replaceAll(regex, (match) => `<mark>${match}</mark>`);
-
-            const category = item.querySelector('.post-date a');
-            category.innerHTML = category.innerHTML.replaceAll(regex, (match) => `<mark>${match}</mark>`);
-
-            const description = item.querySelector('.post-description');
-            description.innerHTML = description.innerHTML.replaceAll(regex, (match) => `<mark>${match}</mark>`);
-          });
-        }
+        return regex.test(page.title)
+          || regex.test(page.description)
+          || regex.test(page.category);
       });
+
+      renderResults(block, searchResults, q);
     }
   });
 }
